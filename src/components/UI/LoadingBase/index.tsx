c50dde700e38ffd1d0214f3789d4eb08010021ac
ducthanh <ducthanhpg@gmail.com>
2025-02-24 23:27:43 +0700

    update code

--- src/components/UI/LoadingBase/index.tsx
+++ src/components/UI/LoadingBase/index.tsx
@@ -4,9 +4,9 @@ const LoadingBase = ({ loading }: { loading: boolean }) => {
       {loading && (
         <div className="flex items-center justify-center min-h-screen bg-primary">
           <div className="flex space-x-6">
-            <div className="w-6 h-6 bg-blue-500 rounded-full animate-ping"></div>
-            <div className="w-6 h-6 bg-blue-500 rounded-full animate-ping200"></div>
-            <div className="w-6 h-6 bg-blue-500 rounded-full animate-ping400"></div>
+            <div className="w-5 h-5 bg-blue-500 rounded-full animate-ping"></div>
+            <div className="w-5 h-5 bg-blue-500 rounded-full animate-ping200"></div>
+            <div className="w-5 h-5 bg-blue-500 rounded-full animate-ping400"></div>
           </div>
         </div>
       )}
