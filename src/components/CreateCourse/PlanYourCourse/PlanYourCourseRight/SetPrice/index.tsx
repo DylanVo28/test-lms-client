7080c891c07f74ccd2d7929415607a4b5acfaa85
ducthanh <ducthanhpg@gmail.com>
2025-03-06 21:49:18 +0700

    update code

--- src/components/CreateCourse/PlanYourCourse/PlanYourCourseRight/SetPrice/index.tsx
+++ src/components/CreateCourse/PlanYourCourse/PlanYourCourseRight/SetPrice/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
+import { Control, Controller, useWatch, useForm } from 'react-hook-form';
 import InputText from '@/components/UI/InputText';
 import Text from '@/components/UI/Text';
-import { Control, Controller, useWatch } from 'react-hook-form';
 import { useTranslation } from 'next-i18next';
 
 const SetPrice = ({ control }: { control: Control }) => {
   const { t } = useTranslation('common');
+  const { setError, clearErrors, formState } = useForm(); // Lấy các hàm hỗ trợ từ react-hook-form
   const originPrice = useWatch({ control, name: 'originPrice' });
+  const finalPrice = useWatch({ control, name: 'price' });
+
+  useEffect(() => {
+    if (Number(finalPrice) > Number(originPrice)) {
+      setError('price', {
+        type: 'validate',
+        message: t('Final Price cannot be greater than Origin Price'),
+      });
+    } else {
+      clearErrors('price');
+    }
+  }, [originPrice, finalPrice, setError, clearErrors, t]);
 
   return (
     <div className="flex flex-col gap-8">
@@ -13,6 +27,7 @@ const SetPrice = ({ control }: { control: Control }) => {
         {t('Set Price')}
       </Text>
       <div className="flex flex-col gap-6">
+        {/* Origin Price */}
         <div className="grid md:grid-cols-6 items-center gap-4">
           <div className="w-[150px] flex items-center gap-1">
             <Text type="font-16-600" className="text-white">
@@ -27,45 +42,6 @@ const SetPrice = ({ control }: { control: Control }) => {
               rules={{
                 required: t('Origin Price is required'),
                 min: { value: 0, message: t('Price must be at least 0') },
-              }}
-              render={({ field, fieldState }) => {
-                return (
-                  <InputText
-                    endContent={
-                      <Text type="font-16-400" className="text-white">
-                        USD
-                      </Text>
-                    }
-                    error={fieldState?.error?.message}
-                    type="number"
-                    onChange={field.onChange}
-                    value={field.value}
-                    className="md:min-w-[600px]"
-                    placeholder={t('0')}
-                    inputDefault
-                  />
-                );
-              }}
-            />
-          </div>
-        </div>
-        <div className="grid md:grid-cols-6 items-center gap-4">
-          <div className="flex items-center gap-1 w-[150px]">
-            <Text type="font-16-600" className="text-white">
-              {t('Final Price')}
-            </Text>
-            <Text className="font-16-400 text-danger"> &nbsp;*</Text>
-          </div>
-          <div className="col-span-4">
-            <Controller
-              name="price"
-              control={control}
-              rules={{
-                required: t('Final Price is required'),
-                min: { value: 0, message: t('Price must be at least 0') },
-                validate: (value) =>
-                  Number(value) <= Number(originPrice) ||
-                  t('Final Price cannot be greater than Origin Price'),
               }}
               render={({ field, fieldState }) => (
                 <InputText
@@ -86,6 +62,45 @@ const SetPrice = ({ control }: { control: Control }) => {
             />
           </div>
         </div>
+
+        {/* Final Price */}
+        <div className="grid md:grid-cols-6 items-center gap-4">
+          <div className="flex items-center gap-1 w-[150px]">
+            <Text type="font-16-600" className="text-white">
+              {t('Final Price')}
+            </Text>
+            <Text className="font-16-400 text-danger"> &nbsp;*</Text>
+          </div>
+          <div className="col-span-4">
+            <Controller
+              name="price"
+              control={control}
+              rules={{
+                required: t('Final Price is required'),
+              }}
+              render={({ field, fieldState }) => {
+                return (
+                  <InputText
+                    endContent={
+                      <Text type="font-16-400" className="text-white">
+                        USD
+                      </Text>
+                    }
+                    error={formState.errors.price?.message as string}
+                    type="number"
+                    onChange={field.onChange}
+                    value={field.value}
+                    className="md:min-w-[600px]"
+                    placeholder={t('0')}
+                    inputDefault
+                  />
+                );
+              }}
+            />
+          </div>
+        </div>
+
+        {/* Promotion Period */}
         <div className="grid md:grid-cols-6 items-center gap-4">
           <div className="w-[150px]">
             <Text type="font-16-600" className="text-white">
@@ -118,4 +133,5 @@ const SetPrice = ({ control }: { control: Control }) => {
     </div>
   );
 };
+
 export default SetPrice;
