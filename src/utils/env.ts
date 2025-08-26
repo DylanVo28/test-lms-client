fdd13b51fa1ff489683fe03227908b215cfac74a
tranntuanduong <tranntuanduong@gmail.com>
2025-08-26 16:38:07 +0700

    build

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,7 @@
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
-
-const ENV = publicRuntimeConfig;
+const ENV = {
+  NODE_ENV: process.env.NODE_ENV || 'production',
+  APP_API_URL:
+    process.env.NEXT_PUBLIC_APP_API_URL || process.env.APP_API_URL || '',
+};
 
 export { ENV };
