a68303a762e9bf084bc4e2084581c66bd275decd
tranntuanduong <tranntuanduong@gmail.com>
2025-08-27 11:32:11 +0700

    vercel

--- src/components/Course/DetailCourse/BreadCrumbs/index.tsx
+++ src/components/Course/DetailCourse/BreadCrumbs/index.tsx
@@ -9,7 +9,7 @@ import { useTranslation } from 'next-i18next';
 const BreadCrumbs = () => {
   const { navigate } = useNavigate();
   const { t } = useTranslation('common');
-
+  // test
   return (
     <div className="flex items-center gap-1">
       <Button
